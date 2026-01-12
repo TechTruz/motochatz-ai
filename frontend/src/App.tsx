16f08124318075a5a26eba164d7b8de1149fd5a6
wayanpratama38 <wayanpratama38@gmail.com>
2026-01-13 06:41:49 +0700

    feat(ui): make playground page and related components
    for the PDF Content part it's still placeholder, while Playground created like dashboar since i can't make the chat section height same as the pdf reader section. So i make the layout same as root dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,16 @@
 import { Route, Routes, Navigate } from "react-router";
 import Dashboard from "./pages/Dashboard";
-
+import Playground from "./pages/Playground"
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Navigate to="/knowledge-base" replace />} />
         <Route path="/knowledge-base" element={<Dashboard />} />
+        <Route path="/playground" element={<Playground />} />
       </Routes>
     </>
   );
 }
 
-  export default App;
+export default App;
